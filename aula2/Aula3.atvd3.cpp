31423fffa36f3780b5a05c207fb7394973cd8e7d
Luiz Fernando Machado Andrade <129541757+LuizFernandoM@users.noreply.github.com>
2026-03-05 20:20:33 -0300

    .\aula2\Aula3.atvd3.cpp

---
+++ aula2/Aula3.atvd3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int *protocolo_urgente;
+
+    protocolo_urgente = (int*) malloc(sizeof(int));
+
+    *protocolo_urgente = 999;
+
+    printf("Valor do protocolo urgente: %d\n", *protocolo_urgente);
+
+    free(protocolo_urgente);
+
+    return 0;
+}
