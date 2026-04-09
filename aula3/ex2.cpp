e5d0a9561814333ad39eb6c1945935c2b802820d
Luiz Fernando Machado Andrade <129541757+LuizFernandoM@users.noreply.github.com>
2026-04-09 20:50:43 -0300

    atualizacao

--- aula3/ex2.cpp
+++ aula3/ex2.cpp
@@ -1,38 +1,47 @@
 #include <stdio.h>
 
 int main() {
-    int cpfs_inativos[11] = {11, 22, 33, 44, 55, 66, 77, 88, 99, 110};
+    int cpfs_inativos[20] = {11, 22, 33, 44, 55, 66, 77, 88, 99, 110};
     int tamanho = 10;
-    int novo = 25;
+    int novo;
+//aqui é inserido o novo valor
+    printf("Digite o novo CPF a ser inserido: ");
+    scanf("%d", &novo);
 
+    // 1. Descobrir a posição correta
+    int pos = 0;
+    while (pos < tamanho && cpfs_inativos[pos] < novo) {
+        pos++;
+    }
 
-    for (int i = tamanho; i > 2; i--) {
+    // 2. Empurrar os elementos pra direita
+    for (int i = tamanho; i > pos; i--) {
         cpfs_inativos[i] = cpfs_inativos[i - 1];
     }
 
-    cpfs_inativos[2] = novo;
+    // 3. Inserir o novo valor
+    cpfs_inativos[pos] = novo;
     tamanho++;
 
-
-    printf("Array atualizado:\n");
+    // Mostrar o array atualizado
+    printf("\nArray atualizado:\n");
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", cpfs_inativos[i]);
     }
 
+    // 🔎 Busca binária
     printf("\n\n--------------busca binaria------------\n");
 
-    int alvo = 25;
-    int passos_binaria = 0;
-    int encontro_binaria = 0;
-    int inicio = 0;
-    int fim = tamanho - 1;
+    int alvo = novo;
+    int inicio = 0, fim = tamanho - 1;
+    int passos = 0, encontrou = 0;
 
     while (inicio <= fim) {
-        passos_binaria++;
+        passos++;
         int meio = (inicio + fim) / 2;
 
         if (cpfs_inativos[meio] == alvo) {
-            encontro_binaria++;
+            encontrou = 1;
             break;
         } else if (cpfs_inativos[meio] > alvo) {
             fim = meio - 1;
@@ -41,8 +50,8 @@ int main() {
         }
     }
 
-    if (encontro_binaria == 1) {
-        printf("O CPF %d foi encontrado com %d passos\n", alvo, passos_binaria);
+    if (encontrou) {
+        printf("CPF %d encontrado em %d passos\n", alvo, passos);
     } else {
         printf("CPF nao encontrado\n");
     }
