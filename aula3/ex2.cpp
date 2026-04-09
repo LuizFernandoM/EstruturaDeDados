9bb4cc7af1aab2a913900d7ad1f74eb6c39d8ef5
Luiz Fernando Machado Andrade <129541757+LuizFernandoM@users.noreply.github.com>
2026-04-09 20:57:34 -0300

    atualizacao2

--- aula3/ex2.cpp
+++ aula3/ex2.cpp
@@ -3,36 +3,37 @@
 int main() {
     int cpfs_inativos[20] = {11, 22, 33, 44, 55, 66, 77, 88, 99, 110};
     int tamanho = 10;
-    int novo;
-//aqui é inserido o novo valor
+    int novo, alvo;
+
+    // insere um novo valor
     printf("Digite o novo CPF a ser inserido: ");
     scanf("%d", &novo);
 
-    // 1. Descobrir a posição correta
+    //escolhe a melhor posiçao
     int pos = 0;
     while (pos < tamanho && cpfs_inativos[pos] < novo) {
         pos++;
     }
 
-    // 2. Empurrar os elementos pra direita
+    //abre espaço pra novo valor
     for (int i = tamanho; i > pos; i--) {
         cpfs_inativos[i] = cpfs_inativos[i - 1];
     }
 
-    // 3. Inserir o novo valor
     cpfs_inativos[pos] = novo;
     tamanho++;
 
-    // Mostrar o array atualizado
+    //mostra a tabela atualizada
     printf("\nArray atualizado:\n");
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", cpfs_inativos[i]);
     }
 
-    // 🔎 Busca binária
-    printf("\n\n--------------busca binaria------------\n");
+    //busca
+    printf("\n\nDigite o CPF que deseja buscar: ");
+    scanf("%d", &alvo);
 
-    int alvo = novo;
+    // Busca binária
     int inicio = 0, fim = tamanho - 1;
     int passos = 0, encontrou = 0;
 
@@ -43,9 +44,11 @@ int main() {
         if (cpfs_inativos[meio] == alvo) {
             encontrou = 1;
             break;
-        } else if (cpfs_inativos[meio] > alvo) {
+        }
+        else if (cpfs_inativos[meio] > alvo) {
             fim = meio - 1;
-        } else {
+        }
+        else {
             inicio = meio + 1;
         }
     }
@@ -53,7 +56,7 @@ int main() {
     if (encontrou) {
         printf("CPF %d encontrado em %d passos\n", alvo, passos);
     } else {
-        printf("CPF nao encontrado\n");
+        printf("CPF %d nao encontrado\n", alvo);
     }
 
     return 0;
